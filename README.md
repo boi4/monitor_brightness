6a3a7130ed0ba85bd69091bd3d2ca1ba6c229467
Jan Fecht <jan.fecht@tum.de>
2020-09-26 15:52:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This little script will change the brightness of the monitor where the cursor/mouse is currently active.
+
+This also works with external monitors that support the DDC protocol (which most of them do).
+
+Dependencies are listed in the script.
---
+++ monitor_brightness.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+"""
+Requirements:
+    - xdotool
+    - xrandr
+    - ddcutil
+    - light
+"""
+import re
+import subprocess
+import argparse
+
+from collections import namedtuple
+
+
+LAPTOP_MONITOR = "eDP-1"
+SUDO = "sudo"
+
+Monitor = namedtuple("Monitor", ["name", "EDID"])
+
+
+def get_active_monitor() -> Monitor:
+    args = ["xdotool", "getmouselocation", "--shell"]
+    r = subprocess.run(args, stdout=subprocess.PIPE)
+    x = re.search(r"X=(\d*)\n", r.stdout.decode('utf-8')).groups()[0]
+    x = int(x)
+    y = re.search(r"Y=(\d*)\n", r.stdout.decode('utf-8')).groups()[0]
+    y = int(y)
+
+    args = ["xrandr", "--verbose"]
+    r = subprocess.run(args, stdout=subprocess.PIPE)
+    xrandr = r.stdout.decode('utf-8')
+
+    monitors = []
+    for line in xrandr.split('\n')[1:]:
+        if not line.startswith("\t") and not line.startswith(" "):
+            monitors.append([])
+        monitors[-1].append(line)
+
+    matches = []
+    for monitor in monitors:
+        m = re.match(
+            r"^([\w-]*) connected.* (\d+)x(\d+)\+(\d+)\+(\d+) .*$", monitor[0])
+        if m is not None:
+            name, xlen, ylen, x0, y0 = m.groups()
+            if name == LAPTOP_MONITOR:
+                return Monitor(name, "")
+            name, xlen, ylen, x0, y0 = name, int(
+                xlen), int(ylen), int(x0), int(y0)
+            if x0 <= x < x0+xlen and y0 <= y < y0+ylen:
+                pos1 = [i for (i, line) in enumerate(monitor)
+                        if line.strip() == "EDID:"][0] + 1
+                pos2 = [i for (i, line) in enumerate(monitor) if not re.match(
+                    r"[0-9a-f]{32}", line.strip()) and i > pos1][0]
+                edid = "".join(line.strip() for line in monitor[pos1:pos2])
+                return Monitor(name=name, EDID=edid)
+
+
+def get_brightness_feature(monitor: Monitor):
+    args = [SUDO, "ddcutil", "-e", monitor.EDID[:256], "capabilities"]
+    r = subprocess.run(args, stdout=subprocess.PIPE)
+    output = r.stdout.decode('utf-8')
+    m = re.search(r"Feature: (\d+) \(Brightness\)", output)
+    if m is not None:
+        feature_val = int(m.groups()[0])
+        return feature_val
+
+
+def get_laptop():
+    args = ["light"]
+    r = subprocess.run(args, stdout=subprocess.PIPE)
+    return float(r.stdout.decode('utf-8').strip())
+
+
+def get_ddc(monitor: Monitor):
+    feature = get_brightness_feature(monitor)
+    args = [SUDO, "ddcutil", "-e", monitor.EDID[:256], "getvcp", str(feature)]
+    r = subprocess.run(args, stdout=subprocess.PIPE)
+    m = re.search(r"current\s+value\s*=\s*(\d*.?\d*),",
+                  r.stdout.decode('utf-8'))
+    if m is not None:
+        return float(m.groups()[0])
+
+
+def get_brightness(monitor: Monitor):
+    if monitor.name == LAPTOP_MONITOR:
+        return get_laptop()
+    else:
+        return get_ddc(monitor)
+
+
+def set_laptop(value: int):
+    args = [SUDO, "light", "-S", str(value)]
+    subprocess.run(args)
+
+
+def set_ddc(monitor: Monitor, value: int):
+    feature = get_brightness_feature(monitor)
+    subprocess.run([SUDO, "modprobe", "i2c-dev"])
+    args = [SUDO, "ddcutil", "-e", monitor.EDID[:256],
+            "setvcp", str(feature), str(value)]
+    subprocess.run(args, stdout=subprocess.PIPE)
+
+
+def set_brightness(monitor: Monitor, value: int):
+    if monitor.name == LAPTOP_MONITOR:
+        return set_laptop(value)
+    else:
+        return set_ddc(monitor, value)
+
+
+def print_brightness_cli(args):
+    monitor = get_active_monitor()
+    print(get_brightness(monitor))
+
+
+def set_brightness_cli(args):
+    monitor = get_active_monitor()
+    set_brightness(monitor, args.value)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(prog="DDC Brightness utility")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-m", "--monitor", type=str,
+                        help="name of the monitor to set brightness on, WIP not implemented yet")
+    subparsers = parser.add_subparsers(
+        title="subcommands", description="valid subcommands", required=True, dest="subcommand")
+
+    parser_get = subparsers.add_parser(
+        "get", help="get active monitor brightness via ddc")
+    parser_get.set_defaults(func=print_brightness_cli)
+
+    parser_set = subparsers.add_parser(
+        "set", help="set active monitor brightness via ddc")
+    parser_set.add_argument("value", type=int, help="brightness value")
+    parser_set.set_defaults(func=set_brightness_cli)
+
+    return parser
+
+
+def main():
+    args = create_parser().parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
